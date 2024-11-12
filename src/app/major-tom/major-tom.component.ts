2d00c4f540880e3b645c9d46d5e748263d11d16b
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-11-12 17:18:00 +0100

    Featuring Teste Push to Github

---
+++ src/app/major-tom/major-tom.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-major-tom',
+  template: '<h3>Major Tom to Ground Control</h3>',
+  styles: []
+})
+export class MajorTomComponent {} 
